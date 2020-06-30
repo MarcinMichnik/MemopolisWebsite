f345c8d20c8a790ad40a3b5c3e6470366a81252f
MarcinMichnik <marcin.michnik@o2.pl>
2020-06-30 18:08:28 +0200

    url fixes

--- memopolis/urls.py
+++ memopolis/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('', MemeListView.as_view(), name="memopolis-index"),
     path('top/', TopMemeListView.as_view(), name="memopolis-top"),
     path('poczekalnia/', UnacceptedMemeListView.as_view(), name="memopolis-unaccepted"),
-    path('meme/<int:pk>', MemeDetailView.as_view(), name="meme-detail"),
-    path('meme/nowy', MemeCreateView.as_view(), name="meme-create"),
+    path('meme/<int:pk>/', MemeDetailView.as_view(), name="meme-detail"),
+    path('meme/nowy/', MemeCreateView.as_view(), name="meme-create"),
     path('kontakt/', views.kontakt, name="memopolis-kontakt"),
-    path('profil/memy', YourMemesListView.as_view(), name="memopolis-your-memes"),
+    path('profil/memy/', YourMemesListView.as_view(), name="memopolis-your-memes"),
     path('meme/<int:pk>/edytuj/', MemeUpdateView.as_view(), name="meme-edit"),
 ]
